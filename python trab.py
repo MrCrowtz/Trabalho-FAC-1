864d4da7a5a443374328bf3f995d39806ae14a62
MrCrowtz <74215350+MrCrowtz@users.noreply.github.com>
2022-04-18 14:00:37 -0300

    Update python trab.py

--- python trab.py
+++ python trab.py
@@ -2,7 +2,7 @@ def leitura():
     with open('entrada.txt','r') as ler:
         valores=[]
         for linha in open('entrada.txt').readlines():
-            valores.append(read.readlines())
+            valores.append(ler.readlines())
     pares=[]
     for i in range(0,len(valores),2):
         pares.append(valores[i:i+2])
